7687db9477dae8416e77626331d67e4284cfc9d6
Braden Shipley <bradenrshipley@gmail.com>
2018-12-17 15:22:11 -0600

    add solutions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { Provider } from "react-redux"
+import store from "./store"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import registerServiceWorker from "./registerServiceWorker"
+import "./index.css"
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
+registerServiceWorker()
